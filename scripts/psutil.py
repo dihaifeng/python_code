108ad0481b5c05b31432c8fc18256c773e6b6964
dihaifeng <dihaifeng@126.com>
2017-03-29 22:26:18 +0800

    Update

--- scripts/psutil.py
+++ scripts/psutil.py
@@ -4,7 +4,7 @@
 import psutil,time
 
 def getProcessInfo(proc):
-    cpu=int(proc.cpu_percent(interval=0))    #获取CPU使用率
+    cpu=int(proc.cpu_percent(interval=1))    #获取CPU使用率
     rss='%.2f'%proc.memory_percent()         #获取物理内存使用率,单位为GB
     name=proc.name()                         #获取进程名
     pid=proc.pid                             #获取进程PID
@@ -13,17 +13,14 @@ def getProcessInfo(proc):
 
 def getAllProcessInfo():
     instances = []
-    all_processes = list(psutil.process_iter())       #获取当前机器全部的进程
-    for proc in all_processes:
-        proc.cpu_percent(interval=0)
-    time.sleep(1)
+    all_processes = list(psutil.process_iter())       #获取当前机器全部的进程            
     for proc in all_processes:
         instances.append(getProcessInfo(proc))
     return instances
 
 ret=getAllProcessInfo()
 for i in ret:
-    if i[4] == '/' or i[1] == 'sudo' or i[1] =='su':      #过滤掉系统进程已经无关进程
+    if i[4] == '/' or i[1] == 'sudo' or i[1] =='su':      #过滤掉系统进程
         pass
     else:
         print i
