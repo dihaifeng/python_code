ee2cbfa3711b835c44709e05733b9297ef07af11
haifeng <dihaifeng@126.com>
2017-06-05 23:22:14 +0800

    update dir

--- docker/docker_api.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import docker
-
-c = docker.Client(base_url='unix://var/run/docker.sock',version='1.22',timeout=10)
-ex = c.exec_create(container='37d91ab772d3',cmd='mkdir /data0')
-print c.exec_inspect(ex)
-ls=c.exec_start(exec_id=ex["Id"],tty=True)
